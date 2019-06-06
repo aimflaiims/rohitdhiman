3f0052ce3019361c8f3a5649907a9c003d815aa6
aimflaiims <aimflaiims@gmail.com>
2019-06-06 12:56:21 +0530

    Update Portfolio.js

--- imports/ui/Portfolio.js
+++ imports/ui/Portfolio.js
@@ -24,36 +24,57 @@ export default class Portfolio extends Component{
               <div className="ml-auto mr-auto">
                 <div className="row">
                   <div className="col-md-6">
-                    <div className="cc-porfolio-image img-raised" data-aos="fade-up" data-aos-anchor-placement="top-bottom"><a href="https://crazy-snake.herokuapp.com/">
+                    <div className="cc-porfolio-image img-raised" data-aos="fade-up" data-aos-anchor-placement="top-bottom"><a href="https://crazy-snake.herokuapp.com/" target="_blank">
                         <figure className="cc-effect"><img src="images/project-1.jpg" alt="Image" />
                           <figcaption>
                             <div className="h4">Crazy Snake</div>
                             <p>React App</p>
                           </figcaption>
                         </figure></a></div>
-                    <div className="cc-porfolio-image img-raised" data-aos="fade-up" data-aos-anchor-placement="top-bottom"><a href="#web-development">
+                    <div className="cc-porfolio-image img-raised" data-aos="fade-up" data-aos-anchor-placement="top-bottom"><a href="https://devconnector-beta.herokuapp.com/" target="_blank">
                         <figure className="cc-effect"><img src="images/project-2.jpg" alt="Image" />
                           <figcaption>
-                            <div className="h4">Startup Project</div>
-                            <p>Web Development</p>
+                            <div className="h4">Dev Connector</div>
+                            <p>MERN Stack App</p>
                           </figcaption>
                         </figure></a></div>
                   </div>
                   <div className="col-md-6">
-                    <div className="cc-porfolio-image img-raised" data-aos="fade-up" data-aos-anchor-placement="top-bottom"><a href="#web-development">
+                    <div className="cc-porfolio-image img-raised" data-aos="fade-up" data-aos-anchor-placement="top-bottom"><a href="https://pareaonline.com/" target="_blank">
                         <figure className="cc-effect"><img src="images/project-3.jpg" alt="Image" />
                           <figcaption>
-                            <div className="h4">Food Order Project</div>
+                            <div className="h4">Parea Online</div>
                             <p>Web Development</p>
                           </figcaption>
                         </figure></a></div>
                     <div className="cc-porfolio-image img-raised" data-aos="fade-up" data-aos-anchor-placement="top-bottom"><a href="#web-development">
                         <figure className="cc-effect"><img src="images/project-4.jpg" alt="Image" />
                           <figcaption>
-                            <div className="h4">Web Advertising Project</div>
+                            <div className="h4">Aurora</div>
                             <p>Web Development</p>
                           </figcaption>
-                        </figure></a></div>
+                        </figure></a>
+                    </div>
+                    
+                  </div>
+
+                  <div className="col-md-6">
+                    <div className="cc-porfolio-image img-raised" data-aos="fade-up" data-aos-anchor-placement="top-bottom"><a href="#web-development">
+                      <figure className="cc-effect"><img src="images/project-3.jpg" alt="Image" />
+                        <figcaption>
+                          <div className="h4">Abaco</div>
+                          <p>Web Development</p>
+                        </figcaption>
+                      </figure></a></div>
+                    <div className="cc-porfolio-image img-raised" data-aos="fade-up" data-aos-anchor-placement="top-bottom"><a href="#web-development">
+                      <figure className="cc-effect"><img src="images/project-4.jpg" alt="Image" />
+                        <figcaption>
+                          <div className="h4">Aurora</div>
+                          <p>Web Development</p>
+                        </figcaption>
+                      </figure></a>
+                    </div>
+
                   </div>
                 </div>
               </div>
